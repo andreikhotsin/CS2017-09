be922e3a43eabd9736d94d48573ce36707f09244
andreikhotsin <andrei.khotsin@gmail.com>
2017-10-02 21:26:25 +0300

    lesson3-taskA

---
+++ src/by/it/sc03_evening_mon_fri/khotsin/lesson03/TaskA3.java
@@ -0,0 +1,16 @@
+package by.it.sc03_evening_mon_fri.khotsin.lesson03;
+/*
+Измените переменные и строку вывода в программе так,
+чтобы она расчитывала и печатала следующее выражение
+3*3+4*4=25
+ */
+
+class TaskA3 {
+    public static void main(String[] args) {
+        int i = 2;
+        i = i*i;
+        int j = 3;
+        int k = i*i + j*j;
+        System.out.println((j * j)) "+" + i * i + "=" + k;
+    }
+}
