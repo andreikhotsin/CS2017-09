9d4310aed01d0d54cfdd73c7e7a805774ca15dcc
andreikhotsin <andrei.khotsin@gmail.com>
2017-10-06 21:01:59 +0300

    lesson3-taskC1

--- src/by/it/sc03_evening_mon_fri/khotsin/lesson03/TaskC1.java
+++ src/by/it/sc03_evening_mon_fri/khotsin/lesson03/TaskC1.java
@@ -9,8 +9,16 @@ package by.it.sc03_evening_mon_fri.khotsin.lesson03;
 Вывод:
 Sum = 26
 */
+import java.util.Scanner;
+
 class TaskC1 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("enter two integer number");
+        int mysum = keyboard.nextInt();
+        int mysumm = keyboard.nextInt();
 
-
+        System.out.println("Sum = " + (mysum + mysumm));
+    }
 
 }
